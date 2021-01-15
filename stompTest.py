fc09ad71617c933d9cacef348a07b455cd9646a1
patrick <pw00242@surrey.ac.uk>
2021-01-15 23:57:19 +0000

    Code to calulate y=phi x, and print out values to console

--- stompTest.py
+++ stompTest.py
@@ -1,8 +1,10 @@
 # test of stagewise orthogonal matching persuit (Donoho et al.)
 import numpy as np
 
-k = 5
-N = 32
+# parameters of problem: k is sparsity and N is total dimension
+k = 2
+N = 8
+print('k = {}, N = {}\n'.format(k, N))
 
 # create a k-spare N dimensional vector
 x = np.random.rand(N)
@@ -10,6 +12,7 @@ random_indicies = np.random.choice(N, k, replace=False)
 for index in range(N):
     if not index in random_indicies:
         x[index] = 0
+print('x = '+str(x)+'\n')
 
 # create random Uniform Spherical Ensemble measurement matrix (cols are random
 # points on unit sphere)
@@ -18,3 +21,8 @@ for col_index in range(N):
     # normalise each column (i.e. make sure each column is point on unit sphere)
     col_norm = np.linalg.norm(phi[:,col_index])
     phi[:,col_index] /= col_norm
+print('phi = '+str(phi)+'\n')
+
+# calculate y (vector of measurements)
+y = np.matmul(phi, x)
+print('y = '+str(y) +'\n')
