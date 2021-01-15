59b1462188659bd35a05145b2e71251ba0cac2e3
patrick <pw00242@surrey.ac.uk>
2021-01-15 23:50:44 +0000

    Added code to generate random Uniform Spherical Ensemble matrix

--- stompTest.py
+++ stompTest.py
@@ -7,8 +7,14 @@ N = 32
 # create a k-spare N dimensional vector
 x = np.random.rand(N)
 random_indicies = np.random.choice(N, k, replace=False)
-for i in range(N):
-    if not i in random_indicies:
-        x[i] = 0
+for index in range(N):
+    if not index in random_indicies:
+        x[index] = 0
 
-print(x)
+# create random Uniform Spherical Ensemble measurement matrix (cols are random
+# points on unit sphere)
+phi = np.random.rand(N,N)
+for col_index in range(N):
+    # normalise each column (i.e. make sure each column is point on unit sphere)
+    col_norm = np.linalg.norm(phi[:,col_index])
+    phi[:,col_index] /= col_norm
